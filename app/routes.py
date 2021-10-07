8d2bb482350108f50403f96b09f8982f6478c653
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-06 19:49:32 -0700

    commenting out hardcoded data

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,36 @@
 from flask import Blueprint, jsonify
 
-books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-@books_bp.route("", methods=["GET"])
+# books = [
+#     Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# ] 
+
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 # @books_bp.route("", methods=["GET"])
 # def handle_books():
 #     books_response = []
 #     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
+#         books_response.append({
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description
+#         })
 #     return jsonify(books_response)
 
 # @books_bp.route("/<book_id>", methods=["GET"])
 # def handle_book(book_id):
-#     book_id = int(book_id)
 #     for book in books:
 #         if book.id == book_id:
-#             return {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description,
-#             }
+#            return {
+#               "id": book.id,
+#               "title": book.title,
+#               "description": book.description
+#           }
