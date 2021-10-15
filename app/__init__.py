2bdd98dff0c96296bf3c2bbcf946d54febce10d0
Zandra Nguyen <zandra.eng@gmail.com>
2021-10-14 23:10:18 -0700

    add two GET request functions

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,10 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # import hello_world_bp variable from file called routes
+    from .routes import hello_world_bp
+
+    # register the variable flask so it knows to use the blueprint for our route
+    app.register_blueprint(hello_world_bp)
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,22 @@
 from flask import Blueprint
 
+# making hello world blueprint
+hello_world_bp = Blueprint("hello_world", __name__)
+
+# define an endpoint
+# create a blueprint decorator, make the route "hello-world" which accept GET request
+
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def get_hello_world():
+    response = "Hello World!"
+    return response
+
+
+# GET method & JSON format
+@hello_world_bp.route("/hello-world/JSON", methods=["GET"])
+def hello_world_json():
+    return{
+        "name": "Zandra",
+        "message": "Hi",
+    }, 200
